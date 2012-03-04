ca982cbbb94f7f24d9006e43eb2fd4db686a3b80
Joseph Gilley <joe.gilley@gmail.com>
2012-03-03 22:37:57 -0500

    Staged RecPrism to have color specification for each face and vertex.

--- src/com/flipflop/game/graphic/RecPrism.java
+++ src/com/flipflop/game/graphic/RecPrism.java
@@ -4,34 +4,32 @@ import static org.lwjgl.opengl.GL11.*;
 
 public class RecPrism implements Drawable {
 
-	public Vec3 center;
-	public Vec3[] v;
-	public Vec3 rot = new Vec3(0.0f, 0.0f, 0.0f);
-	public float degree = 0.0f;
+	// Indices of v to create faces.  Dependent on the order in which v is instantiated.
+	private static int[][] faces = {{2,1,5,6}, {3,0,4,7}, {7,3,2,6}, {0,4,5,1}, {3,0,1,2}, {4,7,6,5}};
+	protected Vec3[] v;
+	protected Vec3[] faceColor = new Vec3[6];
+	protected Vec3[] indexColor = new Vec3[8];
+	protected Vec3 center;
+	protected Vec3 rot = new Vec3(0.0f, 0.0f, 0.0f);
+	protected float degree = 0.0f;
 
-	private static int[][] bunchOfJunk = {{2,1,5,6}, {3,0,4,7}, {7,3,2,6}, {0,4,5,1}, {3,0,1,2}, {4,7,6,5}};
 
 	static {
 	}
 
 	public RecPrism(Vec3 center, Vec3 rot, float width, float height, float depth, float degree) {
 		this.center = center;
-		float depthBy2 = depth / 2;
-		float widthBy2 = width / 2;
-		float heightBy2 = height / 2;
-		v = new Vec3[8];
-		v[0] = new Vec3(( + widthBy2), ( - heightBy2), ( + depthBy2));
-		v[1] = new Vec3(( + widthBy2), ( + heightBy2), ( + depthBy2));
-		v[2] = new Vec3(( - widthBy2), ( + heightBy2), ( + depthBy2));
-		v[3] = new Vec3(( - widthBy2), ( - heightBy2), ( + depthBy2));
-		v[4] = new Vec3(( + widthBy2), ( - heightBy2), ( - depthBy2));
-		v[5] = new Vec3(( + widthBy2), ( + heightBy2), ( - depthBy2));
-		v[6] = new Vec3(( - widthBy2), ( + heightBy2), ( - depthBy2));
-		v[7] = new Vec3(( - widthBy2), ( - heightBy2), ( - depthBy2));
+		this.rot = rot;
+		this.degree = degree;
+		initVertices(width, height, depth);
 	}
 	
 	public RecPrism(Vec3 center, float width, float height, float depth) {
 		this.center = center;
+		initVertices(width, height, depth);
+	}
+	
+	private void initVertices(float width, float height, float depth) {
 		float depthBy2 = depth / 2;
 		float widthBy2 = width / 2;
 		float heightBy2 = height / 2;
@@ -59,19 +57,19 @@ public class RecPrism implements Drawable {
 		glRotatef(this.degree, rot.x, rot.y, rot.z);
 		
 		glBegin(GL_QUADS);
-		for (int i=0; i<bunchOfJunk.length; i++) {
-			for (int j=0; j<bunchOfJunk[i].length; j++) {
-				glVertex3f(v[bunchOfJunk[i][j]].x, v[bunchOfJunk[i][j]].y, v[bunchOfJunk[i][j]].z);
+		for (int i=0; i<faces.length; i++) {
+			for (int j=0; j<faces[i].length; j++) {
+				glVertex3f(v[faces[i][j]].x, v[faces[i][j]].y, v[faces[i][j]].z);
 			}
 		}
 		glEnd();
 
 		glColor3f(0.0f,0.0f,0.0f);
 		
-		for (int i=0; i<bunchOfJunk.length; i++) {
+		for (int i=0; i<faces.length; i++) {
 			glBegin(GL_LINE_LOOP);
-			for (int j=0; j<bunchOfJunk[i].length; j++) {
-				glVertex3f(v[bunchOfJunk[i][j]].x, v[bunchOfJunk[i][j]].y, v[bunchOfJunk[i][j]].z);
+			for (int j=0; j<faces[i].length; j++) {
+				glVertex3f(v[faces[i][j]].x, v[faces[i][j]].y, v[faces[i][j]].z);
 			}
 			glEnd();
 		}
